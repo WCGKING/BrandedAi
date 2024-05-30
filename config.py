98e178c729e2b22b33053f41e2b39cbb964ab859
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-30 11:36:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", 27353035))
 API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
-BOT_TOKEN = getenv("BOT_TOKEN", None)
 OWNER_ID = int(getenv("OWNER_ID", "6752444356"))
 MONGO_URL = getenv("MONGO_URL", None)
 SUPPORT_GRP = getenv("SUPPORT_GRP", "BRANDED_WORLD")
