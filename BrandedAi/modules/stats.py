10c2f198fab6f4113d63f7d2b2d71a307368a660
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-30 10:56:30 +0530

    𝐁𝐫𝐚𝐧𝐝𝐞𝐝 - 𝐀𝐢

---
+++ BrandedAi/modules/stats.py
@@ -0,0 +1,18 @@
+from pyrogram import filters, Client
+from pyrogram.types import Message
+
+from BrandedAi import OWNER, Branded
+from BrandedAi.database.chats import get_served_chats
+from BrandedAi.database.users import get_served_users
+
+
+@Branded.on_message(filters.command("stats") & filters.user(OWNER))
+async def stats(cli: Client, message: Message):
+    users = len(await get_served_users())
+    chats = len(await get_served_chats())
+    await message.reply_text(
+        f"""ᴛᴏᴛᴀʟ sᴛᴀᴛs ᴏғ {(await cli.get_me()).mention} :
+
+➻ **ᴄʜᴀᴛs :** {chats}
+➻ **ᴜsᴇʀs :** {users}"""
+    )
