ee81362c561c1299716a27e142e47eecbd3676ce
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-05-30 11:17:31 +0530

    𝐁𝐫𝐚𝐧𝐝𝐞𝐝 - 𝐀𝐢

---
+++ config.py
@@ -0,0 +1,80 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID", 27353035))
+API_HASH = getenv("API_HASH", "cf2a75861140ceb746c7796e07cbde9e")
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+OWNER_ID = int(getenv("OWNER_ID", "6752444356"))
+MONGO_URL = getenv("MONGO_URL", None)
+SUPPORT_GRP = getenv("SUPPORT_GRP", "BRANDED_WORLD")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "BRANDED_PAID_CC")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "BRANDEDKING82")
+
+# Random Start Images
+IMG = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+]
+
+
+# Random Stickers
+STICKER = [
+    "",
+    "",
+    "",
+]
+
+
+EMOJIOS = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+]
